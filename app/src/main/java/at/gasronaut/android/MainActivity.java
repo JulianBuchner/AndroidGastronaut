70a37a2b908acbd31e9da5baf76ea5083bac7e62
mblaimsc <mblaimsc@htl-steyr.ac.at>
2022-09-30 16:13:58 +0200

    deleted fab and added button to toolbar

--- app/src/main/java/at/gasronaut/android/MainActivity.java
+++ app/src/main/java/at/gasronaut/android/MainActivity.java
@@ -3,6 +3,7 @@ package at.gasronaut.android;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -29,13 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -52,7 +48,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // getMenuInflater().inflate(R.menu.main, menu);
+        Button button = (Button)findViewById(R.id.nav_header_shoppingbasket);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent i = new Intent(this, ActivityStore.class);
+                //startActivity(i);
+            }
+        });
         return true;
     }
 
